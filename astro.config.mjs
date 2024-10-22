a04fb8c68f52817034edec0ff9c983e33ef314e6
OmegaTroy <juli.mjoi44@gmail.com>
2024-10-22 13:52:52 -0300

    install add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
+
   experimental:{
     contentLayer: true
   },
-  integrations: [tailwind()]
+
+  integrations: [tailwind()],
+  adapter: vercel()
 });
